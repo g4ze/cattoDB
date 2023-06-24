a16c7f2cd998a6d2ab74f48d76244125a4b74fc7
g4ze <bhaiyajionline17@gmail.com>
2023-06-24 23:33:57 +0530

    eeeehhh
    cri agya

--- API/controller/controller.go
+++ API/controller/controller.go
@@ -2,18 +2,22 @@ package controller
 
 import (
 	"cattobrain"
-	"encoding/json"
-	"net/http"
+
+	"github.com/labstack/echo"
 )
 
-func ReadOne(w http.ResponseWriter, r *http.Request) {
+// func ReadOne(w http.ResponseWriter, r *http.Request) {
 
-	// controller.Controller( []string)
-	// Process the identifier
-	// You can perform any desired logic with the identifier
-	command := (r.URL.Path[1:])
+// 	// controller.Controller( []string)
+// 	// Process the identifier
+// 	// You can perform any desired logic with the identifier
+// 	command := (r.URL.Path[1:])
+// 	result := cattobrain.MeowAPI(command)
+// 	w.Header().Set("Content-Type", "application/json")
+// 	response, _ := json.Marshal(result)
+// 	w.Write(response)
+// }
+func ReadOne(context echo.Context) error {
+	command := context.Param("id")
 	result := cattobrain.MeowAPI(command)
-	w.Header().Set("Content-Type", "application/json")
-	response, _ := json.Marshal(result)
-	w.Write(response)
 }
